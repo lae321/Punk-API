0978d988f3e4afc2fb9a5e6cfc4fe1a938d26a38
lae321 <lae321@hotmail.co.uk>
2022-02-11 10:02:13 +0000

    Basic styling

--- punk-api/src/App.jsx
+++ punk-api/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.scss";
 import Navbar from "./containers/Navbar/Navbar";
-import Main from "./containers/Main/Main";
+// import Main from "./containers/Main/Main";
 import beers from "./data/beer";
 
 const App = () => {
